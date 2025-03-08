9db61232a41b784f6d918e503bee6af88d99c0f6
sahinton21 <sahinton21@gmail.com>
2025-03-08 15:37:13 -0600

    Set up project for developing stuff

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+// vue.config.js
+//This will use a ternary to redirect us to the right build.
+// we need a build environment variable when running this.
+// All Github pages environment variables must start with VUE_APP_
+const REPO_NAME="congress-tracker"
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/'
+}
